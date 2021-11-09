621a1cac27e073f86269e97bb0fdb8b492b3686f
Paul Coste <paul@lescoste.net>
2021-11-09 01:48:59 +0000

    Fixed indexation

--- file_construct.py
+++ file_construct.py
@@ -2,11 +2,11 @@ import os
 
 year = 2020
 
-# create the directories
+# create the directories with the empty dayX.py and input.txt files 
 for i in range(25):
-    os.makedirs("./"+str(year)+"/day"+str(i+1))
-
-# create the empty dayX.py and input.txt files 
-for i in range(25):
-    open("./"+str(year)+"/day"+str(i+1)+"/day"+str(i+1)+".py", mode='x')
-    open("./"+str(year)+"/day"+str(i+1)+"/input.txt", mode='x')
+    s = str(i+1)
+    if len(s)==1:
+        s = '0'+s
+    os.makedirs("./"+str(year)+"/day"+s)
+    open("./"+str(year)+"/day"+s+"/day"+s+".py", mode='x')
+    open("./"+str(year)+"/day"+s+"/input.txt", mode='x')
